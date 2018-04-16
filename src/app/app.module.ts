0957360fc864bc5cd158ce817339ae9f5a1a4f72
goga koreli <gogakoreli@gmail.com>
2018-04-16 14:23:59 +0400

    restructure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { Injector, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
-import { BetterComponent } from './better.component';
+import { WithLoveComponent } from './with-love/with-love.component';
+
+export const AngularElements = [WithLoveComponent];
 
 @NgModule({
-    declarations: [BetterComponent],
+    declarations: [...AngularElements],
     imports: [BrowserModule],
-    entryComponents: [BetterComponent],
+    entryComponents: [...AngularElements],
 })
 export class AppModule {
     constructor(private injector: Injector) {
-        const customElement = createCustomElement(BetterComponent, {
+        const customElement = createCustomElement(WithLoveComponent, {
             injector,
         });
         customElements.define('with-love', customElement);
---
+++ src/app/with-love/with-love.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'with-love',
+    templateUrl: './with-love.component.html',
+    styleUrls: ['./with-love.component.scss'],
+})
+export class WithLoveComponent implements OnInit {
+    @Input() public name: string;
+
+    @Input() public url: string;
+
+    @Input() public color: string = 'red';
+
+    @Input() public size: number = 1;
+
+    ngOnInit() {
+        if (!this.name || this.name.length === 0) {
+            console.error(`Name attribute must be provided!`);
+        }
+    }
+}
