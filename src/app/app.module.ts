de82cf232632cb001c5a3c6d30eb20329f29b5e7
goga koreli <gogakoreli@gmail.com>
2018-04-16 12:00:58 +0400

    with-love angular element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
+import { Injector, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-
+import { BetterComponent } from './better.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, BetterComponent],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent],
+    entryComponents: [BetterComponent],
+    schemas: [NO_ERRORS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {
+    constructor(private injector: Injector) {
+        const customElement = createCustomElement(BetterComponent, {
+            injector,
+        });
+        customElements.define('with-love', customElement);
+    }
+
+    ngDoBootstrap() {}
+}
